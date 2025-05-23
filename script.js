31b29423bf15b18cb3e6830cd826df39b2c1acf0
abhi3305 <abhinavsharma3305@gmai.com>
2025-05-23 19:12:09 +0530

    nothing much

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ const getUserCoordinates = () =>{
     navigator.geolocation.getCurrentPosition(
         position =>{
             const {latitude, longitude} = position.coords;
-            const REVERSE_GEOCODING_URL = `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
+            const REVERSE_GEOCODING_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
 
         fetch(REVERSE_GEOCODING_URL)
         .then(res => {
